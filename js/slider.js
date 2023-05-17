571a73ab718e22278e62cb56396d0750716a2f2c
ZalikProg <127873845+ZalikProg@users.noreply.github.com>
2023-05-17 19:15:14 +0300

    Add files via upload

---
+++ js/slider.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+    $('.slider').slick({
+        arrows: true,
+        slidesToShow: 1,
+        speed: 800,
+        autoplay: true,
+        waitForAnimate: true,
+        centerMode: true,
+        adaptiveWidth: true,
+    });
+});
